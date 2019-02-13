52eafb64cc0cb61ac60a02ce8fb760f82ad78615
cannoli9917 <katie.pennachio@gmail.com>
2019-02-13 16:30:14 -0500

    changed to search by subject

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,6 +3,8 @@ class Job < ApplicationRecord
   belongs_to :student, class_name: "User", foreign_key: "student_id", optional: true
   belongs_to :tutor, class_name: "User",  foreign_key: "tutor_id", optional: true
   belongs_to :topic
+  delegate :subject, :to => :topic, :allow_nil => true
+
 
 
   def self.looking_for_tutors
@@ -23,9 +25,11 @@ class Job < ApplicationRecord
 
   def self.search(search)
     if search
-    topic = Topic.find_by(name: search)
-      if !!topic
-        self.where(topic_id: topic.id)
+    subject = Subject.find_by(name: search)
+      if !!subject
+        topics = Topic.where(subject_id: subject.id)
+        topic_ids = topics.map {|topic| topic.id}
+        self.where(topic_id: topic_ids)
       else
         Job.all
       end
