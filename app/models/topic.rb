66ffa919ea6ac812644b448daa403276cce318fe
cannoli9917 <katie.pennachio@gmail.com>
2019-02-13 22:08:31 -0500

    order topics alphabetically in new job form

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,9 @@ class Topic < ApplicationRecord
   has_many :user_topics
   has_many :users, through: :user_topics
   has_many :jobs
+
+
+  def self.alphabetical
+    self.all.sort_by { |t| t.name.downcase }
+  end
 end
